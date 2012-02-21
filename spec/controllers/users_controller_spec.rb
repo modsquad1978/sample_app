96fa981912a0cda0078d3a21766c8ea9f4f1cbee
modsquad1978 <mark.odonnell@azzurgroup.com>
2012-02-21 11:32:00 -0500

    Done with sign in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -46,6 +46,17 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Sign up")
     end
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+    
+   # it "should have an email field" do
+   #   get :new
+   #   response.should have_selector("input[email='user[email]'][type='text']")
+   # end
+    
   end
   
   describe "POST 'create'" do
@@ -96,6 +107,11 @@ describe UsersController do
                     post :create, :user => @attr
                     flash[:success].should =~ /welcome to the sample app/i
                   end
+						
+						it "should sign the user in" do
+							post	:create,	:user => @attr
+							controller.should be_signed_in
+						end
              
           end
       
